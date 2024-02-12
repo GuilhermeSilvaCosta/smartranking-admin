70b0c63118f861e352a34d89d0244fffb8db7192
Guilherme Costa <guilhermecosta@MacBook-Pro-de-Guilherme.local>
2024-02-12 16:13:40 -0300

    Add event subscriber

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PlayerSchema } from './interfaces/players/player.schema';
+import { CategorySchema } from './interfaces/categories/category.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://localhost:27017/sr-admin?authSource=sr-admin',
+    ),
+    MongooseModule.forFeature([{ name: 'Category', schema: CategorySchema }]),
+    MongooseModule.forFeature([{ name: 'Player', schema: PlayerSchema }]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Category } from './interfaces/categories/category.interface';
+import { Model } from 'mongoose';
+import { Player } from './interfaces/players/player.interface';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectModel('Category') private readonly categoryModel: Model<Category>,
+    @InjectModel('Player') private readonly playerModel: Model<Player>,
+  ) {}
+
+  private readonly logger = new Logger(AppService.name);
+
+  async createCategory(category: Category): Promise<Category> {
+    try {
+      const { name } = category;
+
+      const found = await this.categoryModel.findOne({ name }).exec();
+      if (found) throw new RpcException(`Category ${name} already exist.`);
+
+      return this.categoryModel.create(category);
+    } catch (err) {
+      this.logger.error(`error: ${JSON.stringify(err)}`);
+      throw new RpcException(err.message);
+    }
   }
 }
