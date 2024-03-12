9e85552c93566a3ed9336d8575b2d12db0733ef0
Guilherme Costa <guilhermecosta@MacBook-Pro-de-Guilherme.local>
2024-03-12 09:37:30 -0300

    Add modules on app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb://localhost:27017/sr-admin?authSource=sr-admin',
     ),
+    CategoriesModule,
   ],
   controllers: [],
   providers: [],
